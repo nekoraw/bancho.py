56d8f27038081248c73829d77b3805d20739a9b8
luis10barbo <luis10barbo2020@gmail.com>
2022-09-13 08:26:43 -0300

    Added missing awaits to send function

--- app/commands.py
+++ app/commands.py
@@ -713,7 +713,7 @@ async def _map(ctx: Context) -> Optional[str]:
 
             for bmap in app.state.cache.beatmapset[bmap.set_id].maps:
                 if webhook_url := app.settings.DISCORD_BANCHO_UPDATES_WEBHOOK:
-                    app.pycord.send_beatmap_status_change(webhook_url, bmap, new_status)
+                    await app.pycord.send_beatmap_status_change(webhook_url, bmap, new_status)
 
                 bmap.status = new_status
                 
@@ -728,7 +728,7 @@ async def _map(ctx: Context) -> Optional[str]:
 
             if bmap.md5 in app.state.cache.beatmap:
                 if webhook_url := app.settings.DISCORD_BANCHO_UPDATES_WEBHOOK:
-                    app.pycord.send_beatmap_status_change(webhook_url, app.state.cache.beatmap[bmap.md5], new_status)
+                    await app.pycord.send_beatmap_status_change(webhook_url, app.state.cache.beatmap[bmap.md5], new_status)
 
                 app.state.cache.beatmap[bmap.md5].status = new_status
 
