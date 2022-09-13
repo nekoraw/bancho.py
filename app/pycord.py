eadf60be067c584fdc41ebbaa774432ef97450fb
luis10barbo <luis10barbo2020@gmail.com>
2022-09-13 08:58:20 -0300

    separated beatmaps from beatmapsets webhook

--- app/pycord.py
+++ app/pycord.py
@@ -1,37 +1,71 @@
 """Discord utilities that use pycord."""
 
+from typing import Union
+
 from discord.colour import Colour
-from app.objects.beatmap import Beatmap, RankedStatus
+from app.objects.beatmap import Beatmap, BeatmapSet, RankedStatus
 from discord import Webhook, Embed
 import aiohttp
 
 
 def create_beatmap_changes_embed(beatmap:Beatmap, new_status:RankedStatus) -> Embed:
-    embed = Embed(title=f"Status de rank do mapa modificado:", color=Colour(0).from_rgb(0, 0, 255))
+
+    # old_status_string = f"{beatmap.status!s}"
+    new_status_string = f"{new_status!s}"
+
+    title = f"Novo mapa adicionado a seção {new_status_string}:"
+    
+    embed = Embed(title=title, color=Colour(0).from_rgb(0, 0, 255))
 
     embed.set_thumbnail(url=f"https://b.ppy.sh/thumb/{beatmap.set_id}l.jpg")
 
     star_rating = f"{int(beatmap.diff*100)/100}🌟"
 
-    old_status_string = f"{beatmap.status!s}"
-    new_status_string = f"{new_status!s}"
-
     embed.add_field(
-        name=f"{beatmap.artist} - {beatmap.title} [{beatmap.version}] ({star_rating})",
-        value=f"Status: {old_status_string} -> {new_status_string}",
+        name=f"{beatmap.artist} - {beatmap.title} [{beatmap.version}]",
+        value=(f"{star_rating} - CS {beatmap.cs} - AR {beatmap.ar} - BPM {beatmap.bpm}\
+        \n LINK: {beatmap.url}"),
         inline=False
     )
 
     return embed
 
-async def send_beatmap_status_change(webhook_url:str, beatmap:Beatmap, new_status:RankedStatus):
+def create_beatmapset_changes_embed(beatmapset:BeatmapSet, new_status:RankedStatus) -> Embed:
+
+    # old_status_string = f"{beatmap.status!s}"
+    new_status_string = f"{new_status!s}"
+
+    title = f"Novo set de mapas adicionado a seção {new_status_string}:"
+    
+    embed = Embed(title=title, color=Colour(0).from_rgb(0, 0, 255))
+
+    embed.set_thumbnail(url=f"https://b.ppy.sh/thumb/{beatmapset.id}l.jpg")
+
+    embed.add_field(
+        name=f"{beatmapset.maps[0].artist} - {beatmapset.maps[0].title} [{beatmapset.maps[0].version}]",
+        value=(f"Número de mapas no set: {len(beatmapset.maps)}\
+        \n LINK: {beatmapset.url}"),
+        inline=False
+    )
+
+    return embed
+
+
+
+async def send_beatmap_status_change(webhook_url:str, beatmap:Beatmap, new_status:RankedStatus) -> None:
     """Send new ranked status from the beatmap to discord."""
+    embed = create_beatmap_changes_embed(beatmap, new_status)
+
     async with aiohttp.ClientSession() as session:
         webhook = Webhook.from_url(webhook_url, session=session)
-        await webhook.send(embed=create_beatmap_changes_embed(beatmap, new_status))
+        await webhook.send(embed=embed)
     
 
+async def send_beatmapset_status_change(webhook_url:str, beatmapset:BeatmapSet, new_status:RankedStatus) -> None:
+    """Send new ranked status from the beatmapset to discord."""
+    embed = create_beatmapset_changes_embed(beatmapset, new_status)
 
-
-    
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(webhook_url, session=session)
+        await webhook.send(embed=embed)
     
