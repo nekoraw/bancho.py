7eda316d567b6dbd800cdd2bfc22d1e3cc506bc6
luis10barbo <luis10barbo2020@gmail.com>
2022-09-13 10:38:39 -0300

    removed uneeded combo null check

--- app/usecases/performance.py
+++ app/usecases/performance.py
@@ -39,7 +39,7 @@ def calculate_performances_std(
         mods = score["mods"] if score["mods"] != None else 0
         acc = score["acc"] if score["acc"] != None else 100.00
         nmisses = score["nmiss"] if score["nmiss"] != None else 0
-        combo = score["combo"] if score["combo"] else -1
+        combo = score["combo"]
 
         print(mods,acc,nmisses,combo)
 
